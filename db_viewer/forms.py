777c302deb9e47f5bee9fb282867265fc728a483
Kostya <tester.testovich0101@gmail.com>
2020-12-09 11:49:40 +0700

    cmv

--- db_viewer/forms.py
+++ db_viewer/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class AgeForm(forms.Form):
-    age_from = forms.IntegerField(label='От', min_value=1)
+    age_from = forms.IntegerField(label='От', min_value=0)
     age_to = forms.IntegerField(label='До', max_value=99)
 
