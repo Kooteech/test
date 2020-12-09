ce4fc5dd7c5be95b980b5062692abad785053707
Kooteech <tester.testovich0101@gmail.com>
2020-12-09 13:11:54 +0700

    Added db_viewer age filter

--- db_viewer/views.py
+++ db_viewer/views.py
@@ -3,12 +3,14 @@ from django_tables2 import RequestConfig
 from dbs_app.models import ChildData
 from db_viewer.forms import AgeForm
 from django.http import HttpResponseRedirect
-from datetime import date
+import datetime
+import time
+from itertools import chain
 
 
 def calculateAge(birthDate):
     days_in_year = 365.2425
-    age = int((date.today() - birthDate).days / days_in_year)
+    age = int((datetime.date.today() - birthDate).days / days_in_year)
     return age
 
 
@@ -18,12 +20,18 @@ def get_childdata(request):
         if form.is_valid():
             age_from = form.cleaned_data.get("age_from")
             age_to = form.cleaned_data.get("age_to")
+            my_obj_list = list()
+            table = ChildData.objects.all()
             if (age_from == 0) & (age_to == 0):
-                table = ChildData.objects.all()
                 context = {'table': table}
-                for data in table:
-                    print(data)
-                return render(request, 'db_viewer/table.html', context)
+            else:
+                for obj in table:
+                    if (calculateAge(obj.date_birth) >= age_from) and (calculateAge(obj.date_birth) <= age_to):
+                        my_obj_list.append(obj)
+                none_qs = ChildData.objects.none()
+                qs = list(chain(none_qs, my_obj_list))
+                context = {'table': qs}
+            return render(request, 'db_viewer/table.html', context)
     else:
         form = AgeForm()
     return render(request, 'db_viewer/search.html', {'form': form})
