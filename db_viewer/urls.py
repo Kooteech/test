00114c0635bebb74eede156eb24af9ad3bbf3723
Kooteech <tester.testovich0101@gmail.com>
2020-12-14 10:44:16 +0700

    Added db_viewer added groups

--- db_viewer/urls.py
+++ db_viewer/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('childdata/', views.get_childdata, name='get_childdata'),
+    path(r'childdata/moreinfo/', views.get_more_info)
+
 ]
--- db_viewer/views.py
+++ db_viewer/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-from django_tables2 import RequestConfig
-from dbs_app.models import ChildData
+from dbs_app.models import ChildData, Group
 from db_viewer.forms import AgeForm
-from django.http import HttpResponseRedirect
 import datetime
-import time
 from itertools import chain
 
 
@@ -23,15 +20,46 @@ def get_childdata(request):
             my_obj_list = list()
             table = ChildData.objects.all()
             if (age_from == 0) & (age_to == 0):
-                context = {'table': table}
+                for obj in table:
+                    if obj.sex == "boy":
+                        obj.sex = "Мальчик"
+                        my_obj_list.append(obj)
+                    else:
+                        my_obj_list.append(obj)
+                        obj.sex = "Девочка"
+                none_qs = ChildData.objects.none()
+                qs = list(chain(none_qs, my_obj_list))
+                context = {'table': qs}
             else:
                 for obj in table:
                     if (calculateAge(obj.date_birth) >= age_from) and (calculateAge(obj.date_birth) <= age_to):
-                        my_obj_list.append(obj)
+                        if obj.sex == "boy":
+                            obj.sex = "Мальчик"
+                            my_obj_list.append(obj)
+                        else:
+                            my_obj_list.append(obj)
+                            obj.sex = "Девочка"
                 none_qs = ChildData.objects.none()
                 qs = list(chain(none_qs, my_obj_list))
                 context = {'table': qs}
             return render(request, 'db_viewer/table.html', context)
     else:
-        form = AgeForm()
+        form = AgeForm(initial={"age_from": 0, "age_to": 0})
     return render(request, 'db_viewer/search.html', {'form': form})
+
+
+def get_more_info(request):
+    my_obj_list = list()
+    if request.method == "GET":
+        child_id = request.GET.get('id')
+        if not child_id:
+            form = AgeForm(initial={"age_from": 0, "age_to": 0})
+            return render(request, 'db_viewer/search.html', {'form': form})
+        else:
+            data = ChildData.objects.get(id=child_id)
+            groups = Group.objects.filter(children=child_id)
+            if data.sex == "boy":
+                data.sex = "Мальчик"
+            else:
+                data.sex = "Девочка"
+            return render(request, 'db_viewer/moreinfo.html', {'data': data, 'groups': groups})
